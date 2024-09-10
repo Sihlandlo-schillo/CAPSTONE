e3d27a9843074351a5937ebb3fa2704e11acb33c
sihlandlo-schillo <sihlandlom@gmail.com>
2024-09-10 10:12:26 +0200

    cookies

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,21 +13,18 @@ const checkUser = async (req,res,next)=>{
     const {email,password} = req.body
 
     try{
-        //Retrieve the use from the database
+        //Retrieve the user from the database
         const user = await loginUserDB(email)
         console.log(user);
         
 
         // check if user exists
         if(!user){
-        return res.status(404).send('User with that email not found, Try adding a correct email address or sign up')
+        return res.status(404).send('Incorrect log in details')
         }
         let hashedPassword = user.password
 
-        console.log(hashedPassword); // can comment out this when i see everything works
-
-        // Compare the provided password with the hashed password in the database
-
+        console.log(hashedPassword); 
         compare(password,hashedPassword,(err,result)=>{
             if (err) {
                 return res.status(500).send('Error comparing passwords')
@@ -35,59 +32,68 @@ const checkUser = async (req,res,next)=>{
 
             if(result==true){
 
-                // 'secret' doesn't what you write there
-
                 let token = jwt.sign({email:email},process.env.SECRET_KEY,{expiresIn:'1h'})
-                // res.json({token})
+            
                 console.log(token);
-                req.body.token = token
+                res.json({ token });
+                // req.body.token = token
                 
                 next()
-               // return //take this 'return' to else
+              
             }else{
-                return res.status(401).send('Invalid password')
-                // res.send('Wrong Password')
+                return res.status(401).send('Incorrect loggin details')
             }
         })
     }catch(err){
         return res.status(500).send('Database error')
     }
-    // let result = compare(password, hashedPassword)
-    // if(result == true){
-    //     next()
-    // }else{
-    //     res.send('Wrong Password Insertion')
-    // }
-
+  
 
 }
 
+// const verifyAToken = (req, res, next) => {
+//     let token;
+
+//     // Check if token exists in cookies
+//     const { cookie } = req.headers;
+//     if (cookie) {
+//         token = cookie.split('=')[1];
+//     }
+
+//     // Check if token exists in Authorization header (Bearer token)
+//     if (!token && req.headers.authorization) {
+//         token = req.headers.authorization.split(' ')[1];
+//     }
+
+//     // If no token is found
+//     if (!token) {
+//         return res.status(403).json({ message: 'Missing Token' });
+//     }
+
+//     // Verify token
+//     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+//         if (err) {
+//             return res.status(403).json({ message: 'Invalid Token or token expired' });
+//         } else {
+//             req.body.user = decoded.email;
+//             console.log(decoded);
+//             next();
+//         }
+//     });
+// };
+
 const verifyAToken = (req, res, next) => {
-    let token;
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1]; // Extract from cookie or header
 
-    // Check if token exists in cookies
-    const { cookie } = req.headers;
-    if (cookie) {
-        token = cookie.split('=')[1];
-    }
-
-    // Check if token exists in Authorization header (Bearer token)
-    if (!token && req.headers.authorization) {
-        token = req.headers.authorization.split(' ')[1];
-    }
-
-    // If no token is found
     if (!token) {
         return res.status(403).json({ message: 'Missing Token' });
     }
 
-    // Verify token
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
-            return res.status(403).json({ message: 'Invalid Token or token expired' });
+            return res.status(403).json({ message: 'Invalid Token/ token expired' });
         } else {
             req.body.user = decoded.email;
-            console.log(decoded);
             next();
         }
     });
