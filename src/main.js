34132dc55b89f9e85cb1f7d894fe2886ba155e40
sihlandlo-schillo <sihlandlom@gmail.com>
2024-09-13 15:16:15 +0200

    main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import VueCookies from 'vue-cookies'
+import vuecookies from 'vue-cookies'
 
 
 
-createApp(App).use(store).use(router).use(VueCookies,{expires: '1h'}).mount('#app')
+createApp(App).use(store).use(router).use(vuecookies,{expires: '1h'}).mount('#app')
