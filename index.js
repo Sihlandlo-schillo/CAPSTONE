22e6b43539664c111ad30877a044710159d74874
sihlandlo-schillo <sihlandlom@gmail.com>
2024-09-13 14:10:15 +0200

    joel's

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ import itemsRouter from './routes/itemsRouter.js'
 let port = process.env.PORT || 6262
 
 const app = express()
-app.use(cors({
-    // BELOW, allow backend to accept information (also on front-end 'store')
-    origin:['https://capstone-4b7d8.web.app/','*'],
-    credentials:true
-}))                                                                                                                                 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next();
+  });
+app.use(cors())                                                                                                                                 
 app.use(express.json())
 
 app.use(express.static('public')) // Don't know what it is for
